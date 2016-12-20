43a46d789f8b4565145dd9863d802d6f3e628396
burla7 <zhangjiaqih@foxmail.com>
2016-12-20 14:02:33 +0800

    测试

--- src/com/qcode/jqzhangl/Singleton/Singleton.java
+++ src/com/qcode/jqzhangl/Singleton/Singleton.java
@@ -8,7 +8,7 @@ import java.io.Serializable;
  */
 public final class Singleton implements Serializable {
 
-    /* 持有私有静态实例，防止被引用，此处赋值为null，目的是实现延迟加载 */
+    /* 持有私有静态实例，         防止被引用，此处赋值为null，目的是实现延迟加载 */
     private static Singleton instance = null;
 
     /* 私有构造方法，防止被实例化 */
